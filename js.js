2f7baca901bcbff71535cd5453ea863de1fc0c1a
FunkAdri <adrienhenry.fp@gmail.com>
2018-10-30 08:44:00 +0100

    fin

--- js.js
+++ js.js
@@ -1,8 +1,1 @@
 
-  angular.module("ouiTest", [])
-    .controller("ouiController", ["$scope", "$window", function($scope, $window) {
-      $scope.oui = "J'avais dis : Ne cliquez pas";
-      $scope.doAlert = function(oui) {
-        $window.alert(oui);
-      };
-    }]);
